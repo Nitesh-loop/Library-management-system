5b5a2b6d5a973eec1e0008a61c264f178b68dc30
Nitesh-loop <nitesh166k@gmail.com>
2025-05-19 00:18:56 +0530

    testing the book.java using LibraryApp.java

--- src/main/java/com/library/presentation/LibraryApp.java
+++ src/main/java/com/library/presentation/LibraryApp.java
@@ -1,4 +1,13 @@
 package com.library.presentation;
 
+import com.library.model.Book;
+
 public class LibraryApp {
+    public static void main(String[] args) {
+        // Create a new Book object
+        Book book1 = new Book("Java Programming", "John Doe", 29.99, "1234567890", 2023);
+
+        // Output the book details
+        System.out.println("Book Details: " + book1.getTitle() + ", " + book1.getAuthor() + ", " + book1.getISBN());
+    }
 }
