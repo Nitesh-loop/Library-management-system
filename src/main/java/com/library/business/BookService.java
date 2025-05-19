746260569e1fd9ecde257e2d9f4ddbe6bcb3c92a
Nitesh-loop <nitesh166k@gmail.com>
2025-05-20 00:46:32 +0530

    add the bussiness layer for Book class as BookService in buiness layer/package

--- src/main/java/com/library/business/BookService.java
+++ src/main/java/com/library/business/BookService.java
@@ -1,4 +1,25 @@
 package com.library.business;
 
+import com.library.model.Book;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookService {
+
+    // books variable of type list with generic type of book only
+    private List<Book> books;
+
+    //constructor
+    public BookService() {
+        books = new ArrayList<>();
+    }
+
+    //get all the books in the list
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    // Get a book by ISBN
+
+
 }
